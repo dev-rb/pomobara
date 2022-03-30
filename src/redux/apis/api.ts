240154530de7fdc11ac20edf993650e235f64c50
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-03-30 14:46:34 -0400

    Fix backend url for deployment.

--- src/redux/apis/api.ts
+++ src/redux/apis/api.ts
@@ -2,7 +2,7 @@ import { PERSIST_REHYDRATE } from "@redux-offline/redux-offline/lib/constants";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IRootState } from "../store";
 
-const BASE_URL = 'http://localhost:5000/';
+const BASE_URL = 'https://pomobara-backend.herokuapp.com/';
 export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: BASE_URL,
